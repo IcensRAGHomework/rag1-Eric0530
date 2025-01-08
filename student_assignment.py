a31aa22249393761d4a0dab2917950863b236ba9
eric7_chen <eric7_chen@asus.com>
2025-01-08 10:41:27 +0800

    PS/Eric7_Chen Homework 1

--- student_assignment.py
+++ student_assignment.py
@@ -9,8 +9,40 @@ from langchain_core.messages import HumanMessage
 gpt_chat_version = 'gpt-4o'
 gpt_config = get_model_configuration(gpt_chat_version)
 
+format_output = """
+{
+    "Result": 
+        {
+            "date": "2024-10-10",
+            "name": "國慶日"
+        }
+}
+"""
 def generate_hw01(question):
-    pass
+    
+    print("generate_hw01 請回答台灣特定月份的紀念日有哪些(請用JSON格式呈現)")
+    print(question)  
+    question += "我希望的輸出格式是符合json格式的如下字串"+ format_output + "，不需要其他說明也不需要轉成markdown，若有多個節日，請放在\"Results\"" 
+    
+       
+    llm = AzureChatOpenAI(
+            model=gpt_config['model_name'],
+            deployment_name=gpt_config['deployment_name'],
+            openai_api_key=gpt_config['api_key'],
+            openai_api_version=gpt_config['api_version'],
+            azure_endpoint=gpt_config['api_base'],
+            temperature=gpt_config['temperature'] 
+    )
+    message = HumanMessage(
+            content=[
+                {"type": "text" , "text": question},
+            ]
+    )
+    response = llm.invoke([message])
+    
+    return response.content
+   
+    
     
 def generate_hw02(question):
     pass
@@ -38,3 +70,7 @@ def demo(question):
     response = llm.invoke([message])
     
     return response
+    
+
+print(generate_hw01("2024年台灣10月紀念日有哪些?"))
+
